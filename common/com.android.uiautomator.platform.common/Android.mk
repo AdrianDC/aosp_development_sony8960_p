41a78eee06e9f395929070e7faac4b027a0c6017
Guang Zhu <guangzhu@google.com>
2015-01-09 20:28:27 -0800

    Only enable build modules for platform branches
    Bug: 18708851
    Change-Id: If7c0b6d9a5c8c53a2f8c606a2725fd592a399aa9

--- common/com.android.uiautomator.platform.common/Android.mk
+++ common/com.android.uiautomator.platform.common/Android.mk
@@ -14,6 +14,9 @@
 
 LOCAL_PATH:= $(call my-dir)
 
+# don't enable this module for unbundled branch (where source should be used)
+ifeq (,$(TARGET_BUILD_APPS))
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := com.android.uiautomator.platform.common
@@ -24,3 +27,4 @@ LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
 
 include $(BUILD_PREBUILT)
 
+endif
