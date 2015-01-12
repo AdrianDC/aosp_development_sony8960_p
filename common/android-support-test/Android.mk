9823c62cd618568b7bc260a3d7cb3302f710271a
Guang Zhu <guangzhu@google.com>
2015-01-12 20:46:04 +0000

    Revert "Only enable build modules for platform branches"
    Will rename the module in frameworks/testing from 'android-support-test'
    to 'android-support-test-src' and all unbundled app branches will then
    use the prebuilt instead
    
    (cherry-picking around blocked automerger)
    
    Bug: 18977419
    Bug: 18708851
    
    This reverts commit 41a78eee06e9f395929070e7faac4b027a0c6017.
    
    Change-Id: Ia216da599f2bbe7ae234fb636c755b08b7be9768
    (cherry picked from commit 2a6f2f885334ca36efdb9899ab6b47fdfc5f4799)

--- common/android-support-test/Android.mk
+++ common/android-support-test/Android.mk
@@ -14,9 +14,6 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-# don't enable this module for unbundled branch (where source should be used)
-ifeq (,$(TARGET_BUILD_APPS))
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := android-support-test
@@ -27,4 +24,3 @@ LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
 
 include $(BUILD_PREBUILT)
 
-endif
