b31e10c04421558569e64c3f275b7577e85f5329
Iliyan Malchev <malchev@google.com>
2016-08-13 23:03:25 -0700

    hidl_test: clean up
    -- Register FooCallback service so it can respond to requests
    -- Add oneway callbacks to FooCallback
    -- Make logging output more uniform (CLIENT, SERVER(Foo),
       SERVER(FooCallback)
    
    b/30843534: oneway methods seem broken
    
    Change-Id: I0d3572d3d071eb4d627f105c0bd62d96b002f786
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#define LOG_TAG "hidl_test"
 #include <android-base/logging.h>
 
 #include <android/hardware/tests/foo/1.0/BnFoo.h>
@@ -22,14 +23,32 @@ using ::android::hardware::hidl_string;
 using ::android::sp;
 
 struct FooCallback : public BnFooCallback {
-    Status heyItsMe(
+    Status heyItsYou(
             const sp<IFooCallback> &cb) override;
+    Status heyItsYouIsntIt(const sp<IFooCallback> &cb, heyItsYouIsntIt_cb _cb) override;
+    Status heyItsTheMeaningOfLife(uint8_t tmol) override;
 };
 
-Status FooCallback::heyItsMe(
-        const sp<IFooCallback> &cb) {
-    ALOGI("SERVER: heyItsMe cb = %p", cb.get());
+Status FooCallback::heyItsYou(
+        const sp<IFooCallback> &_cb) {
+    ALOGI("SERVER(FooCallback) heyItsYou cb = %p", _cb.get());
+    return Status::ok();
+}
 
+Status FooCallback::heyItsYouIsntIt(const sp<IFooCallback> &_cb,
+        heyItsYouIsntIt_cb _hidl_cb) {
+    ALOGI("SERVER(FooCallback) heyItsYouIsntIt cb = %p sleeping for 10 seconds", _cb.get());
+    sleep(10);
+    ALOGI("SERVER(FooCallback) heyItsYouIsntIt cb = %p responding", _cb.get());
+    if (_hidl_cb)
+      _hidl_cb(1);
+    return Status::ok();
+}
+
+Status FooCallback::heyItsTheMeaningOfLife(uint8_t tmol) {
+    ALOGI("SERVER(FooCallback) heyItsTheMeaningOfLife = %d sleeping for 10 seconds", tmol);
+    sleep(10);
+    ALOGI("SERVER(FooCallback) heyItsTheMeaningOfLife = %d done sleeping", tmol);
     return Status::ok();
 }
 
@@ -79,14 +98,14 @@ struct Bar : public BnBar {
 };
 
 Status Bar::doThis(float param) {
-    ALOGI("Bar::doThis(%.2f)", param);
+    ALOGI("SERVER(Bar) doThis(%.2f)", param);
 
     return Status::ok();
 }
 
 Status Bar::doThatAndReturnSomething(
         int64_t param, doThatAndReturnSomething_cb _cb) {
-    ALOGI("Bar::doThatAndReturnSomething(%ld)", param);
+    ALOGI("SERVER(Bar) doThatAndReturnSomething(%ld)", param);
 
     _cb(666);
 
@@ -99,7 +118,7 @@ Status Bar::doQuiteABit(
         float c,
         double d,
         doQuiteABit_cb _cb) {
-    ALOGI("Bar::doQuiteABit(%d, %ld, %.2f, %.2f)", a, b, c, d);
+    ALOGI("SERVER(Bar) doQuiteABit(%d, %ld, %.2f, %.2f)", a, b, c, d);
 
     _cb(666.5);
 
@@ -108,7 +127,7 @@ Status Bar::doQuiteABit(
 
 Status Bar::doSomethingElse(
         const int32_t param[15], doSomethingElse_cb _cb) {
-    ALOGI("Bar::doSomethingElse(...)");
+    ALOGI("SERVER(Bar) doSomethingElse(...)");
 
     int32_t result[32] = { 0 };
     for (size_t i = 0; i < 15; ++i) {
@@ -125,7 +144,7 @@ Status Bar::doSomethingElse(
 
 Status Bar::doStuffAndReturnAString(
         doStuffAndReturnAString_cb _cb) {
-    ALOGI("Bar::doStuffAndReturnAString");
+    ALOGI("SERVER(Bar) doStuffAndReturnAString");
 
     hidl_string s;
     s = "Hello, world";
@@ -137,7 +156,7 @@ Status Bar::doStuffAndReturnAString(
 
 Status Bar::mapThisVector(
         const hidl_vec<int32_t> &param, mapThisVector_cb _cb) {
-    ALOGI("Bar::mapThisVector");
+    ALOGI("SERVER(Bar) mapThisVector");
 
     hidl_vec<int32_t> out;
     out.resize(param.size());
@@ -153,11 +172,18 @@ Status Bar::mapThisVector(
 
 Status Bar::callMe(
         const sp<IFooCallback> &cb) {
-    ALOGI("Bar::callMe %p", cb.get());
+    ALOGI("SERVER(Bar) callMe %p", cb.get());
 
     if (cb != NULL) {
-        sp<IFooCallback> my_cb = new FooCallback;
-        cb->heyItsMe(my_cb);
+        ALOGI("SERVER(Bar) callMe %p calling IFooCallback::heyItsYou, should return immediately", cb.get());
+        cb->heyItsYou(cb);
+        uint8_t answer = false;
+        ALOGI("SERVER(Bar) callMe %p calling IFooCallback::heyItsYouIsntIt, should block for 10 seconds", cb.get());
+        cb->heyItsYouIsntIt(cb, [&answer](uint8_t response) {answer = response;});
+        ALOGI("SERVER(Bar) callMe %p IFooCallback::heyItsYouIsntIt responded with %d", cb.get(), answer);
+        ALOGI("SERVER(Bar) callMe %p calling IFooCallback::heyItsTheMeaningOfLife, should return immediately", cb.get());
+        cb->heyItsTheMeaningOfLife(42);
+        ALOGI("SERVER(Bar) callMe %p After call to IFooCallback::heyItsTheMeaningOfLife", cb.get());
     }
 
     return Status::ok();
@@ -165,7 +191,7 @@ Status Bar::callMe(
 
 Status Bar::useAnEnum(
         SomeEnum param, useAnEnum_cb _cb) {
-    ALOGI("Bar::useAnEnum %d", (int)param);
+    ALOGI("SERVER(Bar) useAnEnum %d", (int)param);
 
     _cb(SomeEnum::goober);
 
@@ -173,25 +199,25 @@ Status Bar::useAnEnum(
 }
 
 Status Bar::haveAGooberVec(const hidl_vec<Goober>& param) {
-    ALOGI("Bar::haveAGooberVec &param = %p", &param);
+    ALOGI("SERVER(Bar) haveAGooberVec &param = %p", &param);
 
     return Status::ok();
 }
 
 Status Bar::haveAGoober(const Goober &g) {
-    ALOGI("Bar::haveaGoober g=%p", &g);
+    ALOGI("SERVER(Bar) haveaGoober g=%p", &g);
 
     return Status::ok();
 }
 
 Status Bar::haveAGooberArray(const Goober lots[20]) {
-    ALOGI("Bar::haveAGooberArray lots = %p", lots);
+    ALOGI("SERVER(Bar) haveAGooberArray lots = %p", lots);
 
     return Status::ok();
 }
 
 Status Bar::haveATypeFromAnotherFile(const Abc &def) {
-    ALOGI("Bar::haveATypeFromAnotherFile def=%p", &def);
+    ALOGI("SERVER(Bar) haveATypeFromAnotherFile def=%p", &def);
 
     return Status::ok();
 }
@@ -199,7 +225,7 @@ Status Bar::haveATypeFromAnotherFile(const Abc &def) {
 Status Bar::haveSomeStrings(
         const hidl_string array[3],
         haveSomeStrings_cb _cb) {
-    ALOGI("Bar::haveSomeStrings([\"%s\", \"%s\", \"%s\"])",
+    ALOGI("SERVER(Bar) haveSomeStrings([\"%s\", \"%s\", \"%s\"])",
           array[0].c_str(),
           array[1].c_str(),
           array[2].c_str());
@@ -216,7 +242,7 @@ Status Bar::haveSomeStrings(
 Status Bar::haveAStringVec(
         const hidl_vec<hidl_string> &vector,
         haveAStringVec_cb _cb) {
-    ALOGI("Bar::haveAStringVec([\"%s\", \"%s\", \"%s\"])",
+    ALOGI("SERVER(Bar) haveAStringVec([\"%s\", \"%s\", \"%s\"])",
           vector[0].c_str(),
           vector[1].c_str(),
           vector[2].c_str());
@@ -233,7 +259,7 @@ Status Bar::haveAStringVec(
 }
 
 Status Bar::thisIsNew() {
-    ALOGI("Bar::thisIsNew");
+    ALOGI("SERVER(Bar) thisIsNew");
 
     return Status::ok();
 }
@@ -271,31 +297,35 @@ static void client() {
     sp<IFoo> foo = IFoo::asInterface(service);
     CHECK(foo != NULL);
 
+    ALOGI("CLIENT call doThis.");
     foo->doThis(1.0f);
+    ALOGI("CLIENT doThis returned.");
 
-    ALOGI("=> doThis returned.");
-
+    ALOGI("CLIENT call doThatAndReturnSomething.");
     foo->doThatAndReturnSomething(
             2.0f, [&](auto result) {
-                ALOGI("=> doThatAndReturnSomething returned %d.", result);
+                ALOGI("CLIENT doThatAndReturnSomething returned %d.", result);
             });
 
+    ALOGI("CLIENT call doQuiteABit");
     foo->doQuiteABit(
             1, 2, 3.0f, 4.0, [&](auto something) {
-                ALOGI("=> doQuiteABit returned %f.", something);
+                ALOGI("CLIENT doQuiteABit returned %f.", something);
             });
 
+    ALOGI("CLIENT call doSomethingElse");
     int32_t param[15];
     for (size_t i = 0; i < sizeof(param) / sizeof(param[0]); ++i) {
         param[i] = i;
     }
     foo->doSomethingElse(param, [&](const auto &something) {
-            ALOGI("=> doSomethingElse returned %s.",
+            ALOGI("CLIENT doSomethingElse returned %s.",
                   arraylikeToString(something, 32).c_str());
         });
 
+    ALOGI("CLIENT call doStuffAndReturnAString");
     foo->doStuffAndReturnAString([&](const auto &something) {
-            ALOGI("=> doStuffAndReturnAString returned '%s'.",
+            ALOGI("CLIENT doStuffAndReturnAString returned '%s'.",
                   something.c_str());
         });
 
@@ -304,17 +334,19 @@ static void client() {
     for (size_t i = 0; i < 10; ++i) {
         vecParam[i] = i;
     }
-
     foo->mapThisVector(vecParam, [&](const auto &something) {
-            ALOGI("=> mapThisVector returned %s.",
+            ALOGI("CLIENT mapThisVector returned %s.",
                   vecToString(something).c_str());
         });
 
-    foo->callMe(new FooCallback);
-    ALOGI("=> callMe returned.");
+    ALOGI("CLIENT call callMe.");
+    sp<IBinder> cbService = defaultServiceManager()->getService(String16("foo callback"), kVersion);
+    foo->callMe(IFooCallback::asInterface(cbService));
+    ALOGI("CLIENT callMe returned.");
 
+    ALOGI("CLIENT call userAnEnum.");
     foo->useAnEnum(IFoo::SomeEnum::quux, [&](auto sleepy) {
-                ALOGI("=> useAnEnum returned %u", (unsigned)sleepy);
+                ALOGI("CLIENT useAnEnum returned %u", (unsigned)sleepy);
             });
 
     hidl_vec<IFoo::Goober> gooberVecParam;
@@ -322,46 +354,54 @@ static void client() {
     gooberVecParam[0].name = "Hello";
     gooberVecParam[1].name = "World";
 
+    ALOGI("CLIENT call haveAGooberVec.");
     foo->haveAGooberVec(gooberVecParam);
-    ALOGI("=> haveAGooberVec returned.");
+    ALOGI("CLIENT haveAGooberVec returned.");
 
+    ALOGI("CLIENT call haveaGoober.");
     foo->haveAGoober(gooberVecParam[0]);
-    ALOGI("=> haveaGoober returned.");
+    ALOGI("CLIENT haveaGoober returned.");
+
+    ALOGI("CLIENT call haveAGooberArray.");
     IFoo::Goober gooberArrayParam[20];
     foo->haveAGooberArray(gooberArrayParam);
-    ALOGI("=> haveAGooberArray returned.");
+    ALOGI("CLIENT haveAGooberArray returned.");
 
+    ALOGI("CLIENT call haveATypeFromAnotherFile.");
     Abc abcParam{};
     abcParam.x = "alphabet";
     abcParam.y = 3.14f;
     abcParam.z = new native_handle_t();
     foo->haveATypeFromAnotherFile(abcParam);
-    ALOGI("=> haveATypeFromAnotherFile returned.");
+    ALOGI("CLIENT haveATypeFromAnotherFile returned.");
     delete abcParam.z;
     abcParam.z = NULL;
 
+    ALOGI("CLIENT call haveSomeStrings.");
     hidl_string stringArrayParam[3];
     stringArrayParam[0] = "What";
     stringArrayParam[1] = "a";
     stringArrayParam[2] = "disaster";
     foo->haveSomeStrings(stringArrayParam);
-    ALOGI("haveSomeStrings returned.");
+    ALOGI("CLIENT haveSomeStrings returned.");
 
+    ALOGI("CLIENT call haveAStringVec.");
     hidl_vec<hidl_string> stringVecParam;
     stringVecParam.resize(3);
     stringVecParam[0] = "What";
     stringVecParam[1] = "a";
     stringVecParam[2] = "disaster";
     foo->haveAStringVec(stringVecParam);
-    ALOGI("haveAStringVec returned.");
+    ALOGI("CLIENT haveAStringVec returned.");
 
     // Now the tricky part, get access to the derived interface.
 
     sp<IBar> bar = IBar::asInterface(service);
     CHECK(bar != NULL);
 
+    ALOGI("CLIENT call thisIsNew.");
     bar->thisIsNew();
-    ALOGI("thisIsNew returned.");
+    ALOGI("CLIENT thisIsNew returned.");
 }
 
 int main() {
@@ -377,11 +417,24 @@ int main() {
         return 0;
     }
 
-    sp<Bar> bar = new Bar;
+    if (fork() == 0) {
+        ALOGI("SERVER(FooCallback) registering");
+        sp<FooCallback> fcb = new FooCallback;
+        const hidl_version kVersion = make_hidl_version(1, 0);
+        defaultServiceManager()->addService(String16("foo callback"), fcb, kVersion);
+        ALOGI("SERVER(FooCallback) starting");
+        ProcessState::self()->setThreadPoolMaxThreadCount(0);
+        ProcessState::self()->startThreadPool();
+        IPCThreadState::self()->joinThreadPool();
 
+        return 0;
+    }
+
+    ALOGI("SERVER(Bar) registering");
+    sp<Bar> bar = new Bar;
     const hidl_version kVersion = make_hidl_version(1, 0);
     defaultServiceManager()->addService(String16("foo"), bar, kVersion);
-
+    ALOGI("SERVER(Bar) starting");
     ProcessState::self()->setThreadPoolMaxThreadCount(0);
     ProcessState::self()->startThreadPool();
     IPCThreadState::self()->joinThreadPool();
