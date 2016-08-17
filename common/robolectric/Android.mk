d537f6643316951b0faa583dc8b847bda399b371
Daniel Nishi <dhnishi@google.com>
2016-08-17 10:00:47 -0700

    Update the Robolectric makefile to include all Shadows.
    It turns out that the shadows-core jar by itself isn't enough to
    get functional shadows at compilation. We need the version specific
    jars as well.
    
    Change-Id: I4b2ce0a93bf62b9c3e0aed2d8e1297b7de0e65b1

--- common/robolectric/Android.mk
+++ common/robolectric/Android.mk
@@ -9,6 +9,13 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     platform-robolectric-multidex \
     platform-robolectric-resources \
     platform-robolectric-shadows-core \
+    platform-robolectric-shadows-core-v16 \
+    platform-robolectric-shadows-core-v17 \
+    platform-robolectric-shadows-core-v18 \
+    platform-robolectric-shadows-core-v19 \
+    platform-robolectric-shadows-core-v21 \
+    platform-robolectric-shadows-core-v22 \
+    platform-robolectric-shadows-core-v23 \
     platform-robolectric-shadows-httpclient \
     platform-robolectric-snapshot \
     platform-robolectric-utils
@@ -28,7 +35,14 @@ LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
     platform-robolectric-annotations:lib/robolectric-annotations-3.1.1.jar \
     platform-robolectric-multidex:lib/shadows-multidex-3.1.1.jar \
     platform-robolectric-resources:lib/robolectric-resources-3.1.1.jar \
-    platform-robolectric-shadows-core:lib/shadows-core-3.1.1.jar\
+    platform-robolectric-shadows-core:lib/shadows-core-3.1.1.jar \
+    platform-robolectric-shadows-core-v16:lib/shadows-core-v16-3.1.1.jar \
+    platform-robolectric-shadows-core-v17:lib/shadows-core-v17-3.1.1.jar \
+    platform-robolectric-shadows-core-v18:lib/shadows-core-v18-3.1.1.jar \
+    platform-robolectric-shadows-core-v19:lib/shadows-core-v19-3.1.1.jar \
+    platform-robolectric-shadows-core-v21:lib/shadows-core-v21-3.1.1.jar \
+    platform-robolectric-shadows-core-v22:lib/shadows-core-v22-3.1.1.jar \
+    platform-robolectric-shadows-core-v23:lib/shadows-core-v23-3.1.1.jar \
     platform-robolectric-shadows-httpclient:lib/shadows-httpclient-3.1.1.jar\
     platform-robolectric-snapshot:lib/robolectric-3.1.1.jar \
     platform-robolectric-utils:lib/robolectric-utils-3.1.1.jar
