8f49294db5ac2898bac7c58ca20ff37d4cacdf5e
Zhuoyao Zhang <zhuoyao@google.com>
2016-09-28 14:27:56 -0700

    Add instrumentation code on proxy side.
    * Only works for C++ backend for now.
    
    Bug: 31266145
    Test: make hidl_test
    Change-Id: I18a78b274448fee12a8d340a891ba0c4f7a3a738

--- test/data/android/hardware/tests/expression/1.0/BnExpression.h
+++ test/data/android/hardware/tests/expression/1.0/BnExpression.h
@@ -10,21 +10,30 @@ namespace expression {
 namespace V1_0 {
 
 struct BnExpression : public ::android::hardware::BnInterface<IExpression, IHwExpression> {
-  explicit BnExpression(const ::android::sp<IExpression> &_hidl_impl);
+    explicit BnExpression(const ::android::sp<IExpression> &_hidl_impl);
 
-  ::android::status_t onTransact(
-      uint32_t _hidl_code,
-      const ::android::hardware::Parcel &_hidl_data,
-      ::android::hardware::Parcel *_hidl_reply,
-      uint32_t _hidl_flags = 0,
-      TransactCallback _hidl_cb = nullptr) override;
+    ::android::status_t onTransact(
+            uint32_t _hidl_code,
+            const ::android::hardware::Parcel &_hidl_data,
+            ::android::hardware::Parcel *_hidl_reply,
+            uint32_t _hidl_flags = 0,
+            TransactCallback _hidl_cb = nullptr) override;
 
-  // Methods from IExpression follow.
+    // Methods from IExpression follow.
+    inline ::android::hardware::Return<void> foo1(const hidl_array<int32_t, 1 /* (Constants:CONST_FOO + 1) */>& array)  {
+        return mImpl->foo1(array);
+    };
+    inline ::android::hardware::Return<void> foo2(const hidl_array<int32_t, 13 /* (5 + 8) */>& array)  {
+        return mImpl->foo2(array);
+    };
+    inline ::android::hardware::Return<void> foo3(const hidl_array<int32_t, 20 /* Constants:MAX_ARRAY_SIZE */>& array)  {
+        return mImpl->foo3(array);
+    };
 
-  // for hidl instrumentation.
-  std::vector<InstrumentationCallback> instrumentationCallbacks;
-
-  bool enableInstrumentation = false;
+private:
+    // for hidl instrumentation.
+    std::vector<InstrumentationCallback> mInstrumentationCallbacks;
+    bool mEnableInstrumentation;
 };
 
 }  // namespace V1_0
--- test/data/android/hardware/tests/expression/1.0/BpExpression.h
+++ test/data/android/hardware/tests/expression/1.0/BpExpression.h
@@ -10,12 +10,19 @@ namespace expression {
 namespace V1_0 {
 
 struct BpExpression : public ::android::hardware::BpInterface<IHwExpression> {
-  explicit BpExpression(const ::android::sp<::android::hardware::IBinder> &_hidl_impl);
+    explicit BpExpression(const ::android::sp<::android::hardware::IBinder> &_hidl_impl);
 
-  virtual bool isRemote() const { return true; }
+    virtual bool isRemote() const { return true; }
 
-  // Methods from IExpression follow.
+    // Methods from IExpression follow.
+    ::android::hardware::Return<void> foo1(const hidl_array<int32_t, 1 /* (Constants:CONST_FOO + 1) */>& array)  override;
+    ::android::hardware::Return<void> foo2(const hidl_array<int32_t, 13 /* (5 + 8) */>& array)  override;
+    ::android::hardware::Return<void> foo3(const hidl_array<int32_t, 20 /* Constants:MAX_ARRAY_SIZE */>& array)  override;
 
+private:
+    // for hidl instrumentation.
+    std::vector<InstrumentationCallback> mInstrumentationCallbacks;
+    bool mEnableInstrumentation;
 };
 
 }  // namespace V1_0
