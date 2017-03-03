2ef6b39949680840b2acffa2a46edc0fe8f01d0f
Yifan Hong <elsk@google.com>
2017-03-03 14:46:00 -0800

    Update system/tools/hidl/test/test.sh for minor version uprev.
    Bug: 28825532
    Test: system/tools/hidl/test/test.sh -a
    Test: system/tools/hidl/test/test.sh -u
    Test: system/tools/hidl/test/test.sh
    Change-Id: Ie2f23123dee5a5a82e42d70ac0c9506884a36cff

--- test/test.sh
+++ test/test.sh
@@ -1,39 +1,72 @@
 #!/bin/bash
 
-# Run hidl-gen against errors/syntax/1.0/IEx1.hal to ensure it detects as many
-# syntax errors as possible.
+# Run hidl-gen against interfaces in errors/ to ensure it detects as many
+# errors as possible.
 
-if [ ! -d system/tools/hidl/test/errors/syntax/1.0 ] ; then
-  echo "Where is system/tools/hidl/test/errors/syntax/1.0?";
-  exit 1;
+if [ ! -d system/tools/hidl/test/errors/ ]; then
+    echo "Where is system/tools/hidl/test/errors?";
+    exit 1;
 fi
 
 # TODO(b/33276472)
-if [ ! -d system/libhidl/transport ] ; then
-  echo "Where is system/libhidl/transport?";
-  exit 1;
+if [ ! -d system/libhidl/transport ]; then
+    echo "Where is system/libhidl/transport?";
+    exit 1;
 fi
 
-COMMAND='hidl-gen -Lc++ -rtests:system/tools/hidl/test -randroid.hidl:system/libhidl/transport -o /tmp tests.errors.syntax@1.0'
-EXPECTED='system/tools/hidl/test/errors.output'
-
-if [[ "$@" == "-h" ]]
-then
-    echo "$0 [-h|-u|-n]"
+if [[ "$@" == *"-h"* ]]; then
+    echo "$0 [-h|-u|-a]"
     echo "    (No options)  Run and diff against expected output"
     echo "    -u            Update expected output"
     echo "    -a            Run and show actual output"
     echo "    -h            Show help text"
-elif [[ "$@" == "-u" ]]
-then
-    $COMMAND 2>$EXPECTED;
-    echo Updated.
-elif [[ "$@" == "-a" ]]
-then
-    $COMMAND
-else
-    $COMMAND 2>&1 | diff $EXPECTED -
-    if [ $? -eq 0 ]; then
-        echo Success.
-    fi
+    exit 1
 fi
+
+if [[ "$@" == *"-u"* ]]; then update_files=true; fi
+if [[ "$@" == *"-a"* ]]; then show_output=true; fi
+
+function check() {
+    local "${@}"
+    COMMAND="hidl-gen -Lc++ -rtests:system/tools/hidl/test -randroid.hidl:system/libhidl/transport -o /tmp $package"
+
+    if [ $show_output ]; then
+        echo "Running: $COMMAND"
+        $COMMAND
+        echo
+        return
+    fi
+
+    if [[ ! -z "$contains" ]]; then
+        if [ $update_files ]; then
+            # no files to update
+            return
+        fi
+        $COMMAND 2>&1 | grep "$contains" -q
+        if [ $? -eq 0 ]; then
+            echo "Success for $package."
+        else
+            echo "Fail for $package; output doesn't contain '$contains'"
+        fi
+        return
+    fi
+
+    EXPECTED="system/tools/hidl/test/$filename"
+    if [ $update_files ]; then
+        $COMMAND 2>$EXPECTED;
+        echo "Updated $filename."
+    else
+        $COMMAND 2>&1 | diff $EXPECTED -
+        if [ $? -eq 0 ]; then
+            echo "Success for $package."
+        fi
+    fi
+}
+
+check package="tests.errors.syntax@1.0" filename="errors/syntax.output"
+
+check package="tests.errors.versioning@2.2" \
+    contains="Cannot enforce minor version uprevs for tests.errors.versioning@2.2"
+
+check package="tests.errors.versioning@3.3" \
+    contains="Cannot enforce minor version uprevs for tests.errors.versioning@3.3"
