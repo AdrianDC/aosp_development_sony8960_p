17456f1166edbd709089c95f58217b915468cf1c
Andreas Huber <andih@google.com>
2017-04-13 09:58:06 -0700

    Changed the naming scheme for Treble java libraries (both static and dynamic)
    android.hardware.foo@1.0-java => android.hardware.foo-V1.0-java
    
    Bug: 37207894
    Test: make
    Change-Id: I0f62c49a098400a9798b31d0912f0155bbe75c08
    Merged-In: Ia87b63d5c90772e4620aa9fe3ce431b297be117b

--- test/java_test/Android.mk
+++ test/java_test/Android.mk
@@ -5,11 +5,11 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_MODULE := hidl_test_java_lib
 LOCAL_MODULE_STEM := hidl_test_java
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_JAVA_LIBRARIES :=                         \
-    android.hidl.base@1.0-java                  \
-    android.hardware.tests.baz@1.0-java         \
-    android.hardware.tests.expression@1.0-java  \
-    android.hardware.tests.inheritance@1.0-java
+LOCAL_JAVA_LIBRARIES :=                          \
+    android.hidl.base-V1.0-java                  \
+    android.hardware.tests.baz-V1.0-java         \
+    android.hardware.tests.expression-V1.0-java  \
+    android.hardware.tests.inheritance-V1.0-java
 
 include $(BUILD_JAVA_LIBRARY)
 
@@ -26,7 +26,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES :=                \
 
 LOCAL_REQUIRED_MODULES :=                       \
     hidl_test_java_lib                          \
-    android.hidl.base@1.0-java                  \
-    android.hardware.tests.baz@1.0-java
+    android.hidl.base-V1.0-java                 \
+    android.hardware.tests.baz-V1.0-java
 
 include $(BUILD_PREBUILT)
