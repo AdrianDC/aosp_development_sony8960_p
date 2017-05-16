6d78caf9e097c5b958e1ea79f605f70fe599cb82
Steven Moreland <smoreland@google.com>
2017-05-16 14:51:55 -0700

    Allow proxies to be compared and identified.
    Note, this is not an ABI breakage because the virtual
    destructors being added are already created implicitly.
    
    In this CL, we delete the cached instances of
    BnHw* and and then compare BpHw* objects based on
    their remote pointer.
    
    Bug: 63864326
    Bug: 38348572
    Bug: 32172906
    Test: boot and use a device
    Test: boot a marlin
    Test: hidl_test, hidl_test_java
    
    Merged-In: I7cecd9015231bdf9ad4c039df2e4ecb031077dd7
    Change-Id: I7cecd9015231bdf9ad4c039df2e4ecb031077dd7

--- generateCpp.cpp
+++ generateCpp.cpp
@@ -886,6 +886,7 @@ status_t AST::generateStubHeader(const std::string &outputPath) const {
         << " const std::string& HidlInstrumentor_package,"
         << " const std::string& HidlInstrumentor_interface);"
         << "\n\n";
+    out << "virtual ~" << klassName << "();\n\n";
     out << "::android::status_t onTransact(\n";
     out.indent();
     out.indent();
@@ -1511,6 +1512,11 @@ status_t AST::generateStubSource(
         out << "}\n\n";
     }
 
+    out << klassName << "::~" << klassName << "() ";
+    out.block([&]() {
+        out << "::android::hardware::details::gBnMap.erase(_hidl_mImpl.get());\n";
+    }).endl().endl();
+
     status_t err = generateMethods(out, [&](const Method *method, const Interface *) {
         if (!method->isHidlReserved() || !method->overridesCppImpl(IMPL_STUB_IMPL)) {
             return OK;
@@ -1962,8 +1968,7 @@ status_t AST::generateInterfaceSource(Formatter &out) const {
             out << "return ::android::hardware::details::castInterface<";
             out << iface->localName() << ", "
                 << superType->fqName().cppName() << ", "
-                << iface->getProxyName() << ", "
-                << superType->getProxyFqName().cppName()
+                << iface->getProxyName()
                 << ">(\n";
             out.indent();
             out.indent();
