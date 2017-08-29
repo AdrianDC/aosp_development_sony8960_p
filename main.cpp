2e9da1bcc13e957fda8b39357ad1297d2230e2ac
Jiyong Park <jiyong@google.com>
2017-08-29 14:11:48 +0900

    Build HIDL interfaces for testing with cc_test_library
    Add -t option to hidl-gen to emit cc_test_library when generating
    Android.bp. This is to filter-out the shared libs of HALs for testing
    from the VNDK directory (system/lib/vndk) to save storage.
    
    update-makefile-helper.sh is also updated to set the -t option when
    there exists .hidl_for_test file in the directory for an HAL package.
    
    Bug: 64776708
    Test: no tests.vendor*.so file in /system/lib[64]/vndk on 2017 pixel
    devices
    
    Change-Id: If7f46edd75b149afdd119b3ff9a0a21fc0a55646

--- main.cpp
+++ main.cpp
@@ -54,6 +54,8 @@ struct OutputHandler {
     GenerationFunction generate;
 };
 
+static bool generateForTest = false;
+
 static status_t generateSourcesForFile(
         const FQName &fqName,
         const char *,
@@ -740,7 +742,11 @@ static void generateAndroidBpLibSection(
         const std::set<FQName> &importedPackagesHierarchy) {
 
     // C++ library definition
-    out << "cc_library {\n";
+    if (!generateForTest) {
+        out << "cc_library {\n";
+    } else {
+        out << "cc_test_library {\n";
+    }
     out.indent();
     out << "name: \"" << libraryName << (generateVendor ? "_vendor" : "") << "\",\n"
         << "defaults: [\"hidl-module-defaults\"],\n"
@@ -1308,7 +1314,8 @@ static std::vector<OutputHandler> formats = {
 
 static void usage(const char *me) {
     fprintf(stderr,
-            "usage: %s [-p <root path>] -o <output path> -L <language> (-r <interface root>)+ fqname+\n",
+            "usage: %s [-p <root path>] -o <output path> -L <language> (-r <interface root>)+ [-t] "
+            "fqname+\n",
             me);
 
     fprintf(stderr, "         -h: Prints this menu.\n");
@@ -1319,6 +1326,7 @@ static void usage(const char *me) {
     fprintf(stderr, "         -o <output path>: Location to output files.\n");
     fprintf(stderr, "         -p <root path>: Android build root, defaults to $ANDROID_BUILD_TOP or pwd.\n");
     fprintf(stderr, "         -r <package:path root>: E.g., android.hardware:hardware/interfaces.\n");
+    fprintf(stderr, "         -t: generate build scripts (Android.bp) for tests.\n");
 }
 
 // hidl is intentionally leaky. Turn off LeakSanitizer by default.
@@ -1341,7 +1349,7 @@ int main(int argc, char **argv) {
     }
 
     int res;
-    while ((res = getopt(argc, argv, "hp:o:r:L:")) >= 0) {
+    while ((res = getopt(argc, argv, "hp:o:r:L:t")) >= 0) {
         switch (res) {
             case 'p':
             {
@@ -1394,6 +1402,11 @@ int main(int argc, char **argv) {
                 break;
             }
 
+            case 't': {
+                generateForTest = true;
+                break;
+            }
+
             case '?':
             case 'h':
             default:
@@ -1408,6 +1421,11 @@ int main(int argc, char **argv) {
     if (outputFormat == nullptr) {
         fprintf(stderr,
             "ERROR: no -L option provided.\n");
+        exit(1);
+    }
+
+    if (generateForTest && outputFormat->name() != "androidbp") {
+        fprintf(stderr, "ERROR: -t option is for -Landroidbp only.\n");
         exit(1);
     }
 
