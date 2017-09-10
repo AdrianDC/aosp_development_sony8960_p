1becfe9950f1de04a71304ed941f83579838d1f2
Timur Iskhakov <iskhakovt@google.com>
2017-09-09 23:57:01 -0700

    Refactor hidl_error_test build
    bash scripts that assumes that every directory in test/error_test
    is a test.
    
    Test: hidl_error_test
    Change-Id: Id45d224dba12d96d1fd149d337cf4b22a4f62bf9

---
+++ test/error_test/hidl_error_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "hidl_errr_test.sh hidl-gen_path"
+    exit 1
+fi
+
+readonly HIDL_GEN_PATH=$1
+readonly HIDL_ERROR_TEST_DIR='system/tools/hidl/test/error_test'
+
+for dir in $(ls -d $HIDL_ERROR_TEST_DIR/*/); do
+  package=$(basename $dir)
+  $HIDL_GEN_PATH -L check -r test:$HIDL_ERROR_TEST_DIR test.$package@1.0 >/dev/null 2>&1
+  if [ $? -ne 1 ]; then
+    printf "FAIL: %s\n" $package
+    exit 1
+  fi
+done
