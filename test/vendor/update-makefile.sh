887b84c552cfdb074f1bd97ec1cf219d05a97525
Steven Moreland <smoreland@google.com>
2017-10-09 18:40:58 -0700

    hidl-gen generates Java with Soong.
    Bug: 33420795
    Test: boot device + hidl_test_java
    
    Change-Id: I76ab20a60df9f66b28a7f8970e107dee7853903f

--- test/vendor/update-makefile.sh
+++ test/vendor/update-makefile.sh
@@ -4,7 +4,5 @@ options="-r tests:system/tools/hidl/test/ \
          -r android.hidl:system/libhidl/transport \
          -r android.hardware:hardware/interfaces"
 
-hidl-gen -Lmakefile $options tests.vendor@1.0;
 hidl-gen -Landroidbp $options tests.vendor@1.0;
-hidl-gen -Lmakefile $options tests.vendor@1.1;
 hidl-gen -Landroidbp $options tests.vendor@1.1;
