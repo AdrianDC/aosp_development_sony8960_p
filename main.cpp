8edeecbee5d695fd81f7e477bc022c0d8e27ac36
Jiyong Park <jiyong@google.com>
2017-11-21 10:39:22 +0900

    DO NOT MERGE: android.hardware.graphics.allocator@2.0 is no longer VNDK-SP
    The interface lib has been in VNDK-SP because
    android.hardware.graphics.mapper@1.0 was using it. However, since the
    dependency has gone [1], there is no need keep it in VNDK-SP. The
    VNDK-SP set should be kept as small as possible because libs in VNDK-SP
    are subject to double-loading.
    
    Bug: 69480083
    Test: walleye boots to the UI
    Merged-In: I68550922f155501195cd3ee5c9777383d6e4048c
    Change-Id: I68550922f155501195cd3ee5c9777383d6e4048c
    (cherry picked from commit 6060f04aac658dfcd572ac4e542a1a26b1466442)

--- main.cpp
+++ main.cpp
@@ -405,8 +405,7 @@ bool isHidlTransportPackage(const FQName& fqName) {
 bool isSystemProcessSupportedPackage(const FQName& fqName) {
     // Technically, so is hidl IBase + IServiceManager, but
     // these are part of libhidltransport.
-    return fqName.string() == "android.hardware.graphics.allocator@2.0" ||
-           fqName.string() == "android.hardware.graphics.common@1.0" ||
+    return fqName.string() == "android.hardware.graphics.common@1.0" ||
            fqName.string() == "android.hardware.graphics.mapper@2.0" ||
            fqName.string() == "android.hardware.graphics.mapper@2.1" ||
            fqName.string() == "android.hardware.renderscript@1.0" ||
