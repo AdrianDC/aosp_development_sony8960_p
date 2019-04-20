Scott31393/android_hardware_qcom_media

mm-video-legacy: venc: update allocation-size check for meta-buffer
Metadata buffer |MetadataBufferType|buffer_handle_t| size can be
8 or 16 bytes on 32-bit or 64-bit respectively. Update the check
which always assumes 32-bit size.

bug: 22487196

Based on:
https://android.googlesource.com/platform/hardware/qcom/media/+/7d6e61888e010bbe79878827a3a593699e9a2595

msm8974/mm-video-legacy/vidc/venc/src/omx_video_base.cpp
@@ -2618,7 +2618,7 @@ OMX_ERRORTYPE omx_video::allocate_input_meta_buffer(
                     OMX_U32              bytes)
 {
   unsigned index = 0;
-  if(!bufferHdr || bytes != sizeof(encoder_media_buffer_type))
+  if(!bufferHdr || bytes < sizeof(encoder_media_buffer_type))
   {
     DEBUG_PRINT_ERROR("wrong params allocate_input_meta_buffer Hdr %p len %d",
                      bufferHdr,bytes);

AdrianDC/aosp_development_sony8960_p

msm8974/mm-video-legacy/vidc/venc/src/omx_video_base.cpp
@@ -2631,7 +2631,7 @@ OMX_ERRORTYPE omx_video::allocate_input_meta_buffer(
   // In meta-mode alloc-length is not known conclusively
   // Allow allocation for atleast gralloc metadata handles
   //  and check for size in ETB
-  if(!bufferHdr || bytes != sizeof(VideoGrallocMetadata))
+  if(!bufferHdr || bytes < sizeof(VideoGrallocMetadata))
   {
     DEBUG_PRINT_ERROR("wrong params allocate_input_meta_buffer Hdr %p len %d",
                      bufferHdr,bytes);